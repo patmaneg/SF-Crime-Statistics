7cca38fed2689e79b1f54358177570d1e4931d87
patmaneg <patmaneg@gmail.com>
2020-05-24 18:42:01 +0000

    add data_stream code

--- data_stream.py
+++ data_stream.py
@@ -7,42 +7,80 @@ import pyspark.sql.functions as psf
 
 # TODO Create a schema for incoming resources
 schema = StructType([
+    StructField("address", StringType(), True),
+    StructField("address_type", StringType(), True),
+    StructField("agency_id", StringType(), True),
+    StructField("call_date", StringType(), True),
+    StructField("call_date_time", StringType(), True),
+    StructField("call_time", StringType(), True),
+    StructField("city", StringType(), True),
+    StructField("common_location", StringType(), True),
+    StructField("crime_id", StringType(), True),
+    StructField("disposition", StringType(), True),
+    StructField("offense_date", StringType(), True),
+    StructField("original_crime_type_name", StringType(), True),
+    StructField("report_date", StringType(), True),
+    StructField("state", StringType(), True)
 ])
 
 def run_spark_job(spark):
-
     # TODO Create Spark Configuration
     # Create Spark configurations with max offset of 200 per trigger
     # set up correct bootstrap server and port
     df = spark \
         .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:9192") \
+        .option("subscribe", "police-department-calls") \
+        .option("startingOffsets", "earliest") \
+        .option("maxOffsetsPerTrigger", 200) \
+        .load()
 
     # Show schema for the incoming resources for checks
     df.printSchema()
 
     # TODO extract the correct column from the kafka input resources
     # Take only value and convert it to String
-    kafka_df = df.selectExpr("")
+    kafka_df = df.selectExpr("CAST(value AS STRING)")
 
-    service_table = kafka_df\
-        .select(psf.from_json(psf.col('value'), schema).alias("DF"))\
+    service_table = kafka_df \
+        .select(psf.from_json(psf.col('value'), schema).alias("DF")) \
         .select("DF.*")
 
     # TODO select original_crime_type_name and disposition
-    distinct_table = 
+    distinct_table = service_table \
+        .select(psf.to_timestamp(psf.col("call_date_time")).alias("call_date_time"),
+                psf.col('original_crime_type_name'), 
+                psf.col('disposition'))
+    
+    distinct_table.printSchema()
 
     # count the number of original crime type
-    agg_df = 
+    agg_df = distinct_table \
+        .select(
+            distinct_table.call_date_time,
+            distinct_table.original_crime_type_name,
+            distinct_table.disposition) \
+        .withWatermark("call_date_time", "60 minutes") \
+        .groupBy(
+            psf.window(distinct_table.call_date_time, "10 minutes", "5 minutes"),
+            psf.col('original_crime_type_name')
+    ) \
+    .count()
 
     # TODO Q1. Submit a screen shot of a batch ingestion of the aggregation
     # TODO write output stream
     query = agg_df \
-
-
+        .writeStream \
+        .outputMode("complete") \
+        .format("console") \
+        .start()
+    
     # TODO attach a ProgressReporter
     query.awaitTermination()
 
     # TODO get the right radio code json path
+    """
     radio_code_json_filepath = ""
     radio_code_df = spark.read.json(radio_code_json_filepath)
 
@@ -57,6 +95,7 @@ def run_spark_job(spark):
 
 
     join_query.awaitTermination()
+    """
 
 
 if __name__ == "__main__":
